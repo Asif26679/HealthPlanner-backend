e54a1bff4e472677e5f32c3ae901dfaf389f9251
asif26679 <asif26679@gmail.com>
2025-09-12 10:15:14 +0530

    me

--- src/utils/foodData.js
+++ src/utils/foodData.js
@@ -1,5 +1,5 @@
 // foodData.js
-const foods = [
+export const foods = [
   { name: "Eggs", calories: 78, protein: 6, carbs: 1, fats: 5, type: "Breakfast" },
   { name: "Oats", calories: 150, protein: 5, carbs: 27, fats: 3, type: "Breakfast" },
   { name: "Grilled Chicken", calories: 200, protein: 30, carbs: 0, fats: 8, type: "Lunch" },
@@ -9,5 +9,5 @@ const foods = [
   { name: "Nuts", calories: 160, protein: 6, carbs: 6, fats: 14, type: "Snack" },
   { name: "Fruit", calories: 90, protein: 1, carbs: 23, fats: 0, type: "Snack" },
 ];
-export default foods;
+
 
